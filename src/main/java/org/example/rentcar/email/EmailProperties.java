44b2d958261848685a38fbc2607cfb17b0f9c8f6
haicx2 <haicx88011@gmail.com>
2024-12-03 10:16:51 +0700

    change email sender

--- src/main/java/org/example/rentcar/email/EmailProperties.java
+++ src/main/java/org/example/rentcar/email/EmailProperties.java
@@ -3,9 +3,9 @@ package org.example.rentcar.email;
 public class EmailProperties {
     public static final String DEFAULT_HOST = "smtp.gmail.com";
     public static final int DEFAULT_PORT = 587;
-    public static final String DEFAULT_SENDER = "haicx981@gmail.com";
-    public static final String DEFAULT_USERNAME = "haicx981@gmail.com";
-    public static final String DEFAULT_PASSWORD = "wctvyrcjmngzlvao";
+    public static final String DEFAULT_SENDER = "tle847536@gmail.com  ";
+    public static final String DEFAULT_USERNAME = "tle847536@gmail.com  ";
+    public static final String DEFAULT_PASSWORD = "atoizljgtmojfbfq";
     public static final boolean DEFAULT_AUTH = true;
     public static final boolean DEFAULT_STARTTLS = true;
 }
